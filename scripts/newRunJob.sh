f4683f4be67cc537ea458a2e32dd5e4a944c4a95
Ari Cohen <aricohen@yahoo.com>
2024-05-16 14:49:11 -0700

    new run job

---
+++ scripts/newRunJob.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/srv/www/dnaprodb.usc.edu/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/srv/www/dnaprodb.usc.edu/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/srv/www/dnaprodb.usc.edu/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/srv/www/dnaprodb.usc.edu/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+export PATH="/srv/www/dnaprodb.usc.edu/DeepPBS/dependencies/bin:$PATH"
+export X3DNA='/srv/www/dnaprodb.usc.edu/DeepPBS/x3dna-v2.3-linux-64bit/x3dna-v2.3'
+
+export PATH="/srv/www/dnaprodb.usc.edu/DNAProDB/share:$PATH"
+export PATH="/srv/www/dnaprodb.usc.edu/DNAProDB/share/hbplus:$PATH"
+
+conda activate dnaprodb
+
+readonly PIPELINE_DIR="/srv/www/dnaprodb.usc.edu/DNAProDB"
+
+# Run the Python script with arguments passed to the shell script
+python $PIPELINE_DIR/auto_processStructure.py "$1" "$2"
